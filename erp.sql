aaa03aaf26ba453c8dc81a3a17b71976a19e79dc
Ricardo Inostoza Rebolledo <ricardo.inostroza@actus.cl>
2019-03-13 21:29:51 -0300

    Nueva DB

--- erp.sql
+++ erp.sql
@@ -13,9 +13,21 @@
 
 
 -- Volcando estructura de base de datos para erp
-DROP DATABASE IF EXISTS `erp`;
 CREATE DATABASE IF NOT EXISTS `erp` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `erp`;
+
+-- Volcando estructura para tabla erp.cache
+DROP TABLE IF EXISTS `cache`;
+CREATE TABLE IF NOT EXISTS `cache` (
+  `key` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `value` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `expiration` int(11) NOT NULL,
+  UNIQUE KEY `cache_key_unique` (`key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Volcando datos para la tabla erp.cache: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `cache` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cache` ENABLE KEYS */;
 
 -- Volcando estructura para tabla erp.cliente
 DROP TABLE IF EXISTS `cliente`;
@@ -169,6 +181,24 @@ CREATE TABLE IF NOT EXISTS `inventario_unidad` (
 /*!40000 ALTER TABLE `inventario_unidad` DISABLE KEYS */;
 /*!40000 ALTER TABLE `inventario_unidad` ENABLE KEYS */;
 
+-- Volcando estructura para tabla erp.jobs
+DROP TABLE IF EXISTS `jobs`;
+CREATE TABLE IF NOT EXISTS `jobs` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `queue` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `attempts` tinyint(3) unsigned NOT NULL,
+  `reserved_at` int(10) unsigned DEFAULT NULL,
+  `available_at` int(10) unsigned NOT NULL,
+  `created_at` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `jobs_queue_index` (`queue`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Volcando datos para la tabla erp.jobs: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `jobs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `jobs` ENABLE KEYS */;
+
 -- Volcando estructura para tabla erp.linea
 DROP TABLE IF EXISTS `linea`;
 CREATE TABLE IF NOT EXISTS `linea` (
@@ -198,6 +228,62 @@ CREATE TABLE IF NOT EXISTS `marca` (
 -- Volcando datos para la tabla erp.marca: ~0 rows (aproximadamente)
 /*!40000 ALTER TABLE `marca` DISABLE KEYS */;
 /*!40000 ALTER TABLE `marca` ENABLE KEYS */;
+
+-- Volcando estructura para tabla erp.migrations
+DROP TABLE IF EXISTS `migrations`;
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Volcando datos para la tabla erp.migrations: ~8 rows (aproximadamente)
+/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+	(1, '2014_10_12_000000_create_users_table', 1),
+	(2, '2014_10_12_100000_create_password_resets_table', 1),
+	(3, '2017_09_03_144628_create_permission_tables', 1),
+	(4, '2017_09_11_174816_create_social_accounts_table', 1),
+	(5, '2017_09_26_140332_create_cache_table', 1),
+	(6, '2017_09_26_140528_create_sessions_table', 1),
+	(7, '2017_09_26_140609_create_jobs_table', 1),
+	(8, '2018_04_08_033256_create_password_histories_table', 1);
+/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
+
+-- Volcando estructura para tabla erp.model_has_permissions
+DROP TABLE IF EXISTS `model_has_permissions`;
+CREATE TABLE IF NOT EXISTS `model_has_permissions` (
+  `permission_id` int(10) unsigned NOT NULL,
+  `model_type` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `model_id` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`permission_id`,`model_id`,`model_type`),
+  KEY `model_has_permissions_model_type_model_id_index` (`model_type`,`model_id`),
+  CONSTRAINT `model_has_permissions_permission_id_foreign` FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Volcando datos para la tabla erp.model_has_permissions: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `model_has_permissions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `model_has_permissions` ENABLE KEYS */;
+
+-- Volcando estructura para tabla erp.model_has_roles
+DROP TABLE IF EXISTS `model_has_roles`;
+CREATE TABLE IF NOT EXISTS `model_has_roles` (
+  `role_id` int(10) unsigned NOT NULL,
+  `model_type` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `model_id` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`role_id`,`model_id`,`model_type`),
+  KEY `model_has_roles_model_type_model_id_index` (`model_type`,`model_id`),
+  CONSTRAINT `model_has_roles_role_id_foreign` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Volcando datos para la tabla erp.model_has_roles: ~3 rows (aproximadamente)
+/*!40000 ALTER TABLE `model_has_roles` DISABLE KEYS */;
+INSERT INTO `model_has_roles` (`role_id`, `model_type`, `model_id`) VALUES
+	(1, 'App\\Models\\Auth\\User', 1),
+	(2, 'App\\Models\\Auth\\User', 2),
+	(3, 'App\\Models\\Auth\\User', 3);
+/*!40000 ALTER TABLE `model_has_roles` ENABLE KEYS */;
 
 -- Volcando estructura para tabla erp.movimiento
 DROP TABLE IF EXISTS `movimiento`;
@@ -254,6 +340,40 @@ CREATE TABLE IF NOT EXISTS `parametros` (
 /*!40000 ALTER TABLE `parametros` DISABLE KEYS */;
 /*!40000 ALTER TABLE `parametros` ENABLE KEYS */;
 
+-- Volcando estructura para tabla erp.password_histories
+DROP TABLE IF EXISTS `password_histories`;
+CREATE TABLE IF NOT EXISTS `password_histories` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `password_histories_user_id_foreign` (`user_id`),
+  CONSTRAINT `password_histories_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Volcando datos para la tabla erp.password_histories: ~3 rows (aproximadamente)
+/*!40000 ALTER TABLE `password_histories` DISABLE KEYS */;
+INSERT INTO `password_histories` (`id`, `user_id`, `password`, `created_at`, `updated_at`) VALUES
+	(1, 1, '$2y$10$/GLIFuce4od5Z4Z9eX1ShO5BIKLXTP0mtC9BEin5s3HDpVucxasB.', '2019-03-07 15:30:58', '2019-03-07 15:30:58'),
+	(2, 2, '$2y$10$8xz3eI8nz/Yhy5EoeCp2HOsMnumrMX/nz3OvfAVhQNqvJVslHHTcm', '2019-03-07 15:30:58', '2019-03-07 15:30:58'),
+	(3, 3, '$2y$10$ijV/ic1hOY7/nyLznYOePOqVKrtu0mspORhyuo6RBSchnZnjf7j5C', '2019-03-07 15:30:58', '2019-03-07 15:30:58');
+/*!40000 ALTER TABLE `password_histories` ENABLE KEYS */;
+
+-- Volcando estructura para tabla erp.password_resets
+DROP TABLE IF EXISTS `password_resets`;
+CREATE TABLE IF NOT EXISTS `password_resets` (
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Volcando datos para la tabla erp.password_resets: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `password_resets` DISABLE KEYS */;
+/*!40000 ALTER TABLE `password_resets` ENABLE KEYS */;
+
 -- Volcando estructura para tabla erp.periodo_contable
 DROP TABLE IF EXISTS `periodo_contable`;
 CREATE TABLE IF NOT EXISTS `periodo_contable` (
@@ -269,6 +389,23 @@ CREATE TABLE IF NOT EXISTS `periodo_contable` (
 -- Volcando datos para la tabla erp.periodo_contable: ~0 rows (aproximadamente)
 /*!40000 ALTER TABLE `periodo_contable` DISABLE KEYS */;
 /*!40000 ALTER TABLE `periodo_contable` ENABLE KEYS */;
+
+-- Volcando estructura para tabla erp.permissions
+DROP TABLE IF EXISTS `permissions`;
+CREATE TABLE IF NOT EXISTS `permissions` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `guard_name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Volcando datos para la tabla erp.permissions: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `permissions` DISABLE KEYS */;
+INSERT INTO `permissions` (`id`, `name`, `guard_name`, `created_at`, `updated_at`) VALUES
+	(1, 'view backend', 'web', '2019-03-07 15:30:59', '2019-03-07 15:30:59');
+/*!40000 ALTER TABLE `permissions` ENABLE KEYS */;
 
 -- Volcando estructura para tabla erp.producto
 DROP TABLE IF EXISTS `producto`;
@@ -315,6 +452,82 @@ CREATE TABLE IF NOT EXISTS `proveedor` (
 -- Volcando datos para la tabla erp.proveedor: ~0 rows (aproximadamente)
 /*!40000 ALTER TABLE `proveedor` DISABLE KEYS */;
 /*!40000 ALTER TABLE `proveedor` ENABLE KEYS */;
+
+-- Volcando estructura para tabla erp.roles
+DROP TABLE IF EXISTS `roles`;
+CREATE TABLE IF NOT EXISTS `roles` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `guard_name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `roles_name_index` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Volcando datos para la tabla erp.roles: ~4 rows (aproximadamente)
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` (`id`, `name`, `guard_name`, `created_at`, `updated_at`) VALUES
+	(1, 'administrator', 'web', '2019-03-07 15:30:58', '2019-03-07 15:30:58'),
+	(2, 'Vendedor', 'web', '2019-03-07 15:30:58', '2019-03-07 15:30:58'),
+	(3, 'Cliente', 'web', '2019-03-07 15:30:58', '2019-03-07 15:30:58'),
+	(4, 'bodega', 'web', '2019-03-07 17:23:39', '2019-03-07 17:23:39');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+
+-- Volcando estructura para tabla erp.role_has_permissions
+DROP TABLE IF EXISTS `role_has_permissions`;
+CREATE TABLE IF NOT EXISTS `role_has_permissions` (
+  `permission_id` int(10) unsigned NOT NULL,
+  `role_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`permission_id`,`role_id`),
+  KEY `role_has_permissions_role_id_foreign` (`role_id`),
+  CONSTRAINT `role_has_permissions_permission_id_foreign` FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `role_has_permissions_role_id_foreign` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Volcando datos para la tabla erp.role_has_permissions: ~3 rows (aproximadamente)
+/*!40000 ALTER TABLE `role_has_permissions` DISABLE KEYS */;
+INSERT INTO `role_has_permissions` (`permission_id`, `role_id`) VALUES
+	(1, 1),
+	(1, 2),
+	(1, 4);
+/*!40000 ALTER TABLE `role_has_permissions` ENABLE KEYS */;
+
+-- Volcando estructura para tabla erp.sessions
+DROP TABLE IF EXISTS `sessions`;
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `id` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` int(10) unsigned DEFAULT NULL,
+  `ip_address` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `user_agent` text COLLATE utf8mb4_unicode_ci,
+  `payload` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_activity` int(11) NOT NULL,
+  UNIQUE KEY `sessions_id_unique` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Volcando datos para la tabla erp.sessions: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `sessions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sessions` ENABLE KEYS */;
+
+-- Volcando estructura para tabla erp.social_accounts
+DROP TABLE IF EXISTS `social_accounts`;
+CREATE TABLE IF NOT EXISTS `social_accounts` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `provider` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `provider_id` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` text COLLATE utf8mb4_unicode_ci,
+  `avatar` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `social_accounts_user_id_foreign` (`user_id`),
+  CONSTRAINT `social_accounts_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Volcando datos para la tabla erp.social_accounts: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `social_accounts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `social_accounts` ENABLE KEYS */;
 
 -- Volcando estructura para tabla erp.ubicacion
 DROP TABLE IF EXISTS `ubicacion`;
@@ -390,6 +603,40 @@ CREATE TABLE IF NOT EXISTS `unidad_movimiento` (
 -- Volcando datos para la tabla erp.unidad_movimiento: ~0 rows (aproximadamente)
 /*!40000 ALTER TABLE `unidad_movimiento` DISABLE KEYS */;
 /*!40000 ALTER TABLE `unidad_movimiento` ENABLE KEYS */;
+
+-- Volcando estructura para tabla erp.users
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `first_name` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_name` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `avatar_type` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'gravatar',
+  `avatar_location` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password_changed_at` timestamp NULL DEFAULT NULL,
+  `active` tinyint(3) unsigned NOT NULL DEFAULT '1',
+  `confirmation_code` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `confirmed` tinyint(1) NOT NULL DEFAULT '0',
+  `timezone` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_login_at` timestamp NULL DEFAULT NULL,
+  `last_login_ip` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Volcando datos para la tabla erp.users: ~3 rows (aproximadamente)
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`id`, `uuid`, `first_name`, `last_name`, `email`, `avatar_type`, `avatar_location`, `password`, `password_changed_at`, `active`, `confirmation_code`, `confirmed`, `timezone`, `last_login_at`, `last_login_ip`, `remember_token`, `created_at`, `updated_at`, `deleted_at`) VALUES
+	(1, 'b4d94746-b84d-4e27-9df4-4e7aa2868324', 'Ricardo', 'Administrador', 'admin@admin.com', 'gravatar', NULL, '$2y$10$/GLIFuce4od5Z4Z9eX1ShO5BIKLXTP0mtC9BEin5s3HDpVucxasB.', NULL, 1, '30f7fdcef680ebd82fd00cd0cdd28d57', 1, 'America/New_York', '2019-03-07 16:54:18', '127.0.0.1', 'UU0f9blqagiXmnkKkvxCLyfZj9EEeYydONQDACK0BVLGoWu0x6YUHFm2Lbnv', '2019-03-07 15:30:58', '2019-03-07 16:54:18', NULL),
+	(2, '4bc27ba7-b1bd-4dcf-8961-9802d41bcec6', 'Backend', 'User', 'executive@executive.com', 'gravatar', NULL, '$2y$10$8xz3eI8nz/Yhy5EoeCp2HOsMnumrMX/nz3OvfAVhQNqvJVslHHTcm', NULL, 1, 'e011785f14e9f9767f33c8276c86a66c', 1, NULL, NULL, NULL, NULL, '2019-03-07 15:30:58', '2019-03-07 15:30:58', NULL),
+	(3, 'a438e742-a111-4839-8861-b52731397cf0', 'Default', 'User', 'user@user.com', 'gravatar', NULL, '$2y$10$ijV/ic1hOY7/nyLznYOePOqVKrtu0mspORhyuo6RBSchnZnjf7j5C', NULL, 1, 'a391a506a55e2c68ce26162e86320e80', 1, NULL, NULL, NULL, NULL, '2019-03-07 15:30:58', '2019-03-07 15:30:58', NULL);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 -- Volcando estructura para tabla erp.venta
 DROP TABLE IF EXISTS `venta`;
--- routes/backend/admin.php
+++ routes/backend/admin.php
@@ -8,7 +8,34 @@ use App\Http\Controllers\Backend\BodegaController;
 Route::redirect('/', '/admin/dashboard', 301);
 Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::get('bodega/producto', [BodegaController::class, 'producto_index'])->name('bodega.producto');
+/*
+ * Inicio sub menu bodega
+ */
+Route::get('bodega/producto', 'BodegaController@producto_index')->name('bodega.producto');
 Route::get('bodega/entrada', [BodegaController::class, 'entrada_index'])->name('bodega.entrada');
 Route::get('bodega/salida', [BodegaController::class, 'salida_index'])->name('bodega.salida');
 Route::get('bodega/inventario', [BodegaController::class, 'inventario_index'])->name('bodega.inventario');
+
+/*
+ * Fin sub menu bodega
+ */
+
+
+
+/*
+ * Inicio sub menu Sala de ventas
+ */
+
+/*
+ * Fin sub menu Sala de ventas
+ */
+
+
+
+/*
+ * Inicio sub menu Informes
+ */
+
+/*
+ * Fin sub menu Informes
+ */
