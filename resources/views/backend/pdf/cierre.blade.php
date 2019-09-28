8ff63e6d8a445a3ccdb3a8b0c5299a015f92d2d3
Ricardo Inostroza <rinostroza@pop-os.localdomain>
2019-09-28 12:03:05 -0300

    fix-cierre

--- resources/views/backend/pdf/cierre.blade.php
+++ resources/views/backend/pdf/cierre.blade.php
@@ -155,7 +155,7 @@
                             $t_pago_transferencia += $pago_transferencia;
                             $pago_cheque =  $venta->venta_pago_tipo->where("pago_tipo_id", 4)->first() ? $venta->venta_pago_tipo->where("pago_tipo_id", 4)->first()->monto :0;    
                             $t_pago_cheque += $pago_cheque;
-                            $pago_credito =  $venta->venta_pago_tipo->where("pago_tipo_id", 4)->first() ? $venta->venta_pago_tipo->where("pago_tipo_id", 5)->first()->monto : 0;  
+                            $pago_credito =  $venta->venta_pago_tipo->where("pago_tipo_id", 5)->first() ? $venta->venta_pago_tipo->where("pago_tipo_id", 5)->first()->monto : 0;  
                             $t_pago_credito += $pago_credito;
                         @endphp
                         <tr>
@@ -177,10 +177,10 @@
                                     <td></td>
                                     <td></td>
                                     <td>{{ $ventas->sum('pagado') }}</td>
-                                    <td>{{ $efectivo }}  </td>
-                                    <td>{{ $pago_tarjeta }}  </td>
-                                    <td>{{ $pago_transferencia }}  </td>
-                                    <td>{{ $pago_cheque }}  </td>
+                                    <td>{{ $t_efectivo }}  </td>
+                                    <td>{{ $t_pago_tarjeta }}  </td>
+                                    <td>{{ $t_pago_transferencia }}  </td>
+                                    <td>{{ $t_pago_cheque }}  </td>
                                     <td>{{ $t_pago_credito}} </td>
                                 </tr>
                     </tfoot>
