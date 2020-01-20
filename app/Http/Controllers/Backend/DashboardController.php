01e4ecd65ee4cd394956b94758dffd82cfd2ece3
Ricardo Inostroza <rinostroza@pop-os.localdomain>
2020-01-20 16:30:59 -0300

    asdas

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -14,6 +14,9 @@ class DashboardController extends Controller
      */
     public function index()
     {
+
+
+        
         return view('backend.dashboard');
     }
 }
--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <div class="row">
-        <div class="col">
+        <div class="col-6">
             <div class="card">
                 <div class="card-header">
                     <strong>@lang('strings.backend.dashboard.welcome') {{ $logged_in_user->name }}! CI</strong>
@@ -14,5 +14,19 @@
                 </div><!--card-body-->
             </div><!--card-->
         </div><!--col-->
+
+        <div class="col-6">
+            <div class="card">
+                <div class="card-header">
+                    <strong>@lang('strings.backend.dashboard.welcome') Total ventas Usuario</strong>
+                </div><!--card-header-->
+                <div class="card-body">
+                     
+                </div><!--card-body-->
+            </div><!--card-->
+
+
+        </div><!--col-->
+
     </div><!--row-->
 @endsection
